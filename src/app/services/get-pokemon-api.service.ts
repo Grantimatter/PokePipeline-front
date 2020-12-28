3f74f7a09b66e12271a8a3d217c30939e2ee6548
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-27 22:56:52 -0600

    Added method for getting pokemon moves in GetPokemonAPIService

--- src/app/services/get-pokemon-api.service.ts
+++ src/app/services/get-pokemon-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { mergeMap} from 'rxjs/operators';
 import { PokemonAPI } from '../models/pokemon-api';
 
 @Injectable({
@@ -11,7 +12,20 @@ export class GetPokemonAPIService {
   constructor(private httpClient:HttpClient) { }
 
   getPokemonFromAPI(id:number):Observable<PokemonAPI> {
-    return this.httpClient.get("https://pokeapi.co/api/v2/pokemon/"+id+"/") as Observable<PokemonAPI>;
+    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}/`) as Observable<PokemonAPI>;
+  }
+  
+  // Returns an observable of all moves that a pokemon is capable of knowing
+  getPokemonMoves(id:number):Observable<any>{
+    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).pipe(
+      mergeMap((pokemon:any)=>{
+        const moves:Observable<any>[] = [];
+        for(let move in pokemon.moves){
+          moves.push(this.httpClient.get(pokemon.moves[move].move.url));
+        }
+        return forkJoin(moves);
+      })
+    ) as Observable<any>;
   }
 
 }
