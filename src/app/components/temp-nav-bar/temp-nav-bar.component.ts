a6b68ee3ae5057f71dbba4fd4895ff293236013d
topheryun <46732742+topheryun@users.noreply.github.com>
2020-12-28 19:30:26 -0800

    temp nav bar

---
+++ src/app/components/temp-nav-bar/temp-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-nav-bar',
+  templateUrl: './temp-nav-bar.component.html',
+  styleUrls: ['./temp-nav-bar.component.css']
+})
+export class TempNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
