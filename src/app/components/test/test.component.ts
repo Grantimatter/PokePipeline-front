62f72556c0525b61cbf870b87e473a6724b77c46
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-29 09:04:36 -0600

    Added checks in pokemon test to make sure all pokemon are valid

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon';
-import { PokemonAPI } from 'src/app/models/pokemon-api';
 import { GetPokemonAPIService } from 'src/app/services/get-pokemon-api.service';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-test',
@@ -10,33 +10,44 @@ import { GetPokemonAPIService } from 'src/app/services/get-pokemon-api.service';
 })
 export class TestComponent implements OnInit {
 
-  public pokemon:any;
-  public input:number = 1;
+  public pokemon: any;
+  public input: number = 1;
 
-  constructor(private getPokemonService:GetPokemonAPIService) { }
+  constructor(
+    private getPokemonService: GetPokemonAPIService,
+    private pokemonService: PokemonService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  getPokemonAPI():void {
-    this.getPokemonService.getPokemonFromAPI(this.input).subscribe(
-      (pokemonData:Pokemon)=>{
-        this.pokemon=pokemonData;
-      },
-      ()=>{
-        this.pokemon=null;
-        console.log("Something went wrong. Try again");
-      });
+  getPokemonAPI(): void {
+    if (this.pokemonService.isValidPokemonId(this.input)) {
+      this.getPokemonService.getPokemonFromAPI(this.input).subscribe(
+        (pokemonData: Pokemon) => {
+          this.pokemon = pokemonData;
+        },
+        () => {
+          this.pokemon = null;
+          console.log("Something went wrong. Try again");
+        });
+    }
   }
 
-  getPokemonWithAllMovesAPI():void{
-    this.getPokemonService.getPokemonWithAllMovesAPI(this.input).subscribe(
-      (pokemonData:any)=>{
-        this.pokemon = pokemonData;
-        console.log(this.pokemon);
-      },
-      ()=>{
-        console.log("Failed to get Pokemon with moves");
-      });
+  getPokemonWithAllMovesAPI(): void {
+    if (this.pokemonService.isValidPokemonId(this.input)) {
+      this.getPokemonService.getPokemonWithAllMovesAPI(this.input).subscribe(
+        (pokemonData: Pokemon) => {
+          if (this.pokemonService.isValidPokemon(pokemonData)) {
+            this.pokemon = pokemonData;
+            console.log(this.pokemon);
+          } else {
+            this.pokemon = null;
+          }
+        },
+        () => {
+          console.log("Failed to get Pokemon with moves");
+        });
     }
+  }
 }
