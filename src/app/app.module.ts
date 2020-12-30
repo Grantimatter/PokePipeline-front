a745773e857a7a2f00042de1c511831bf5276b26
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-30 11:46:56 -0600

    Installed FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TempNavBarComponent } from './components/temp-nav-bar/temp-nav-bar.component';
 import { PokemonUtilityModule } from './modules/pokemon-utility/pokemon-utility.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, TestComponent, TempNavBarComponent],
@@ -22,6 +23,7 @@ import { PokemonUtilityModule } from './modules/pokemon-utility/pokemon-utility.
     LoginModule,
     HttpClientModule,
     PokemonUtilityModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
