da34398575d48af456e9bac6af0ff167f1387b64
topheryun <46732742+topheryun@users.noreply.github.com>
2021-01-04 13:22:33 -0800

    small bug fix

--- src/app/modules/trainer-hub/services/pokemon-database/pokemon-database.service.ts
+++ src/app/modules/trainer-hub/services/pokemon-database/pokemon-database.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon/pokemon';
-import { AppSettings } from 'src/app/url/app-settings';
 
 /**
  * Interacts with the database with Pokemon objects.
@@ -18,7 +17,6 @@ export class PokemonDatabaseService {
    * @param pokemon The pokemon to be added into the database
    */
   addPokemonToParty(pokemon:Pokemon) {
-    this.httpClient.post(AppSettings.URL + "/addpokemon", pokemon);
   }
 
 }
