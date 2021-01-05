537af3a250e04fbe9681dd901e958c1432a94d8e
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-05 15:45:44 -0600

    rebuilt with new ec2 dns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: "https://pokeapi.co/api/v2",
-  ec2Url: "http://54.215.135.86:8085/PokePipeline",
+  ec2Url: "http://ec2-54-215-135-86.us-west-1.compute.amazonaws.com:8085/PokePipeline",
   pokemonRange: 649
 };
