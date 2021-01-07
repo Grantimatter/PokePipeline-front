758dba707ef6f01fd29b552b574da84d94e59df1
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-07 17:16:49 -0600

    Untracked some things

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,3 +4,6 @@ export const environment = {
   ec2Url: "http://ec2-54-215-135-86.us-west-1.compute.amazonaws.com:8085/PokePipeline",
   pokemonRange: 649
 };
+
+export const trainerAPIendpoint = environment.ec2Url + '/trainer';
+export const authAPIendpoint = environment.ec2Url + '/auth';
