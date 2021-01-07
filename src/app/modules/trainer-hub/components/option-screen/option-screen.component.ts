9bdb4a240ebb32eddc5a2996789cd362bf54f6f2
topheryun <46732742+topheryun@users.noreply.github.com>
2021-01-07 13:47:36 -0800

    hides choose pokemon button after clicking once.

--- src/app/modules/trainer-hub/components/option-screen/option-screen.component.ts
+++ src/app/modules/trainer-hub/components/option-screen/option-screen.component.ts
@@ -9,27 +9,23 @@ import { PartyService } from '../../services/party/party.service';
   styleUrls: ['./option-screen.component.css'],
 })
 export class OptionScreenComponent implements OnInit {
-  private _subscription_user_name: any;
-  pokemon: Pokemon;
 
-  trainer: TrainerModel = {
-    trainerName: 'Trainer',
-    password: 'pass',
-    email: 'trainer@gmail.com',
-    description: 'Pokemon Master (in training)',
-    profilePicture: null,
-  };
+  pokemon: Pokemon;
+  isHidden:boolean = false;
 
   constructor(private partyService: PartyService) {
-    this._subscription_user_name = this.partyService.pokemon1.subscribe(
-      (value) => {
-        this.pokemon = value;
-      }
-    );
   }
 
   ngOnInit(): void {
     this.getPokemonFromService();
+  }
+
+  unHidePartyButton() {
+    this.isHidden = false;
+  }
+
+  hidePartyButton() {
+    this.isHidden = true;
   }
 
   getPokemonFromService() {
